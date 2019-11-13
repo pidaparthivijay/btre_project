2dca873bec8de47b3609685cc0860a101ad72c5b
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2019-11-13 10:58:39 +0530

    Completed the customization of Admin Screen UI and filtering in those screens

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import Listing
+from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', 'price')
+    list_editable = ('is_published', 'price', 'realtor')
+    search_fields = ('price', 'title', 'zipcode',
+                     'description', 'address', 'city')  # donot add any foreign key
+    list_per_page = 20
+
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Realtor
-admin.site.register(Realtor)
+
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_mvp', 'email',
+                    'phone', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('name', 'phone', 'email')
+    list_editable = ('is_mvp', 'phone', 'email')
+    search_fields = ('phone', 'name', 'description',
+                     'email')  # donot add any foreign key
+    list_per_page = 20
+
+
+admin.site.register(Realtor, RealtorAdmin)
