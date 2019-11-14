4fa049a19d95b351033a7bcdb691598c86741708
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2019-11-14 23:48:33 +0530

    Register and Login Mockup linked

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        # Login User
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        # Register User
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/login.html')
