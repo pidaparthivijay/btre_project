c20bf7dbeb2ae4905687c715643dc11c6656837f
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2019-11-16 13:02:14 +0530

    Send mail functionality completed

--- btre/settings.py
+++ btre/settings.py
@@ -149,4 +149,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'projectNew.mailaddress@gmail.com'
-EMAIL_HOST_PASSWORD = 'c3d3vg18'
+EMAIL_HOST_PASSWORD = '*******'
