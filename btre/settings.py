7666ac6c82070acfaa4af423b76e2910e0f17d9d
Vijay Pidparthi <pidaparthivijayaditya@gmail.com>
2019-11-17 10:22:17 +0530

    Changed Mail Details

--- btre/settings.py
+++ btre/settings.py
@@ -149,7 +149,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'projectNew.mailaddress@gmail.com'
-EMAIL_HOST_PASSWORD = 'c3d3vg18'
+EMAIL_HOST_PASSWORD = ''
 
 try:
     from .local_settings import *
